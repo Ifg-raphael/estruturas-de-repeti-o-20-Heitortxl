832b278c669e48ca7aaa3afcc592970a52706e31
nacckl <nacckleial@gmail.com>
2025-08-26 16:29:09 -0300

    Exercício 20 - Heitor

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,13 @@
 # Sua solução aqui
+massa = float(input())
+contador = 0
+while massa >= 0.5:
+    massa = massa - 0.5 * massa
+    contador += 1
+
+segundostot = 50 * contador
+horas = segundostot// 3600
+minutos = (segundostot % 3600) // 60
+segundos = segundostot % 60
+
+print(f"{horas}h  {minutos}m  {segundos}s")
